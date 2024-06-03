cf416e1719001f6b9082304b2a12062303ac8da7
Mattia Adani <mattia.adani@gmail.com>
2024-06-03 21:09:08 +0200

    fix Plug

--- frontend/src/zones/test.js
+++ frontend/src/zones/test.js
@@ -11,7 +11,7 @@ const Device = props => {
   const data = props.data;
 
   if (data.type == 'Light') return <Light data = {data}/>
-  if (data.type == 'Switch') return <Switch data = {data}/>
+  if (data.type == 'Plug') return <Switch data = {data}/>
   
   return <>
       <div className="col-12 col-sm-6 col-md-4 col-lg-3 mb-2">
