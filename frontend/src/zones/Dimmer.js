5de569d9cf822f0d5b048eec4f830bb3f786b24e
Mattia Adani <mattia.adani@gmail.com>
2024-06-04 21:38:10 +0200

    Support for slider in dimmer

--- frontend/src/zones/Dimmer.js
+++ frontend/src/zones/Dimmer.js
@@ -1,18 +1,17 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { getToken, getUser } from '../utils/common';
-//import { Spinner } from 'react-bootstrap';
+import Slider from '@mui/material/Slider';
+import Box from '@mui/material/Box';
 
-const Dimmer = props => {
-
+const Dimmer = (props) => {
     const data = props.data;
     const [isLoading, setLoading] = useState(false);
     const [state, setState] = useState(data.state);
+    const [brightness, setBrightness] = useState(data.brightness);
     const [error, setError] = useState(null);
 
-    
-    function handleClick(action) {
-        
+    const handleClick = (action) => {
         if (isLoading) return;
 
         const backendUrl = process.env.REACT_APP_BACKEND_URL;
@@ -21,65 +20,123 @@ const Dimmer = props => {
         const user = getUser();
 
         setLoading(true);
-        axios.post(
-          `${backendUrl}${api}`,
-          {
-            username:user.username,
-            device: data.device,
-            type: data.type,
-            action: action
-                
-          }, // object for POST request body
-          {
-            headers: {
-              'Authorization': `${token}`,
-              'Content-Type': 'application/json'
-            }
-          }
-        ).then(
-          response => {
-            setLoading(false);
-            console.log(response);
-            setState(action)
-          }
-        ).catch(error => {
-          setLoading(false);
-          setError(error);
-          console.log(error);
-        }).finally(() => {
-          setLoading(false);
-        });
+        axios
+            .post(
+                `${backendUrl}${api}`,
+                {
+                    username: user.username,
+                    device: data.device,
+                    type: data.type,
+                    action: action,
+                }, // object for POST request body
+                {
+                    headers: {
+                        Authorization: `${token}`,
+                        'Content-Type': 'application/json',
+                    },
+                }
+            )
+            .then((response) => {
+                setLoading(false);
+                console.log(response);
+                setState(action);
+            })
+            .catch((error) => {
+                setLoading(false);
+                setError(error);
+                console.log(error);
+            })
+            .finally(() => {
+                setLoading(false);
+            });
     };
-    
-    var bg = "grey"
-    if (state === 'on') bg = 'yellow'
-    if (state === 'off') bg = 'white'
 
-  return <>
-      <div className="col-12 col-sm-6 col-md-4 col-lg-3 mb-2">
-        <div 
-          className={`panel border border-2 m-1`}
-          style={{backgroundColor: bg}}
-        >
-          <p>{data.name}</p>
-          <>
-          <div 
-            className="btn bg-secondary m-1"
-            onClick={() => {handleClick('on')}}
-          >
-          on
-          </div>                  
-          <div 
-          className="btn bg-secondary m-1"
-          onClick={() => {handleClick('off')}}
-          >
-          off
-        </div>
+
+    const handleSliderChange = (event, value) => {
+      if (isLoading) return;
+
+      const backendUrl = process.env.REACT_APP_BACKEND_URL;
+      const api = '/zones/test/';
+      const token = getToken();
+      const user = getUser();
+
+      setLoading(true);
+      axios
+          .post(
+              `${backendUrl}${api}`,
+              {
+                  username: user.username,
+                  device: data.device,
+                  type: data.type,
+                  action: 'brightness',
+                  value: value,
+              }, // object for POST request body
+              {
+                  headers: {
+                      Authorization: `${token}`,
+                      'Content-Type': 'application/json',
+                  },
+              }
+          )
+          .then((response) => {
+              setLoading(false);
+              console.log(response);
+              setBrightness(value);
+          })
+          .catch((error) => {
+              setLoading(false);
+              setError(error);
+              console.log(error);
+          })
+          .finally(() => {
+              setLoading(false);
+          });
+  };
+
+
+    var bg = 'grey';
+    if (state === 'on') bg = 'yellow';
+    if (state === 'off') bg = 'white';
+
+    return (
+        <>
+            <div className="col-12 col-sm-6 col-md-4 col-lg-3 mb-2">
+                <div
+                    className={`panel border border-2 m-1`}
+                    style={{ backgroundColor: bg }}
+                >
+                    <p>{data.name}</p>
+                    <>
+                        <div
+                            className="btn bg-secondary m-1"
+                            onClick={() => {
+                                handleClick('on');
+                            }}
+                        >
+                            on
+                        </div>
+                        <div
+                            className="btn bg-secondary m-1"
+                            onClick={() => {
+                                handleClick('off');
+                            }}
+                        >
+                            off
+                        </div>
+                        <Box sx={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
+                            <Slider
+                                defaultValue={brightness}
+                                aria-label="Default"
+                                valueLabelDisplay="auto"
+                                onChangeCommitted={handleSliderChange}
+                                sx={{ width: '80%' }}
+                            />
+                        </Box>
+                    </>
+                </div>
+            </div>
         </>
-        </div>            
-      </div>
-  </>
-
-}
+    );
+};
 
 export default Dimmer;
