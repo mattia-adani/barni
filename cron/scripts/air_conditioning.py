ab1ff67416bca14efe571d3b1ea94a14a8c76c57
Mattia Adani <mattia.adani@gmail.com>
2024-06-29 13:32:41 +0200

    air_conditioning_control

---
+++ cron/scripts/air_conditioning.py
@@ -0,0 +1,131 @@
+import os, json, time
+import psycopg2 as db
+import paho.mqtt.client as mqtt
+
+dbconfig = {
+    'host': os.environ.get('POSTGRES_HOST'),
+    'port': os.environ.get('POSTGRES_PORT'),
+    'user': os.environ.get('POSTGRES_USER'),
+    'password': os.environ.get('POSTGRES_PASSWORD'),
+    'database': os.environ.get('POSTGRES_DB')
+}
+
+MQTT_BROKER=os.environ.get('MQTT_BROKER')
+MQTT_PORT=int(os.environ.get('MQTT_PORT'))
+MQTT_TOPIC_FOR_ACTION=os.environ.get('MQTT_TOPIC_FOR_ACTION')
+MQTT_TOPIC_FOR_SYNC=os.environ.get('MQTT_TOPIC_FOR_SYNC')
+
+
+def devices(debug = False):
+
+    response = {}
+
+    try:
+        connection = db.connect(**dbconfig)
+        cursor = connection.cursor()
+
+        query = """
+            select device from devices 
+            where property = 'type' and value = 'AirCondControl'
+        """
+        cursor.execute(query)
+        result = cursor.fetchall()
+
+        response['data'] = []
+
+        for device, in result:
+            response['data'].append(device)
+
+        response['status'] = 'OK'
+        return response
+        
+    except Exception as err:
+        response["status"] = "Error"
+        response["message"] = str(err)
+
+    finally:
+        if debug:
+            print(response)
+        cursor.close()
+        connection.close()
+
+def device_info(device_id, debug = True):
+
+    response = {}
+
+    try:
+        connection = db.connect(**dbconfig)
+        cursor = connection.cursor()
+
+        query = f"""
+            select property, value from devices 
+            where device = '{device_id}'
+        """
+        cursor.execute(query)
+        result = cursor.fetchall()
+
+        response['data'] = {}
+
+        for property, value, in result:
+            response['data'][property] = value
+            
+        response['status'] = 'OK'
+        return response
+        
+    except Exception as err:
+        response["status"] = "Error"
+        response["message"] = str(err)
+
+    finally:
+        if debug:
+            print(response)
+        cursor.close()
+        connection.close()
+
+def switch(device, action):
+    payload = {'device': device, 'action': action, 'value': action}
+    mqtt_message = json.dumps(payload)  # Message to publish
+    print(MQTT_TOPIC_FOR_ACTION, payload)
+    client = mqtt.Client()
+    client.connect(MQTT_BROKER, MQTT_PORT, 60)
+    client.publish(MQTT_TOPIC_FOR_ACTION, mqtt_message)
+    client.disconnect()
+
+def update(device):
+    payload = {'device': device, 'action': 'sync', 'value': 1}
+    mqtt_message = json.dumps(payload)  # Message to publish
+    print(MQTT_TOPIC_FOR_ACTION, payload)
+    client = mqtt.Client()
+    client.connect(MQTT_BROKER, MQTT_PORT, 60)
+    client.publish(MQTT_TOPIC_FOR_ACTION, mqtt_message)
+    client.disconnect()
+
+def main():
+
+    d = devices()
+    for device in d['data']: update(device)
+
+    time.sleep(5)
+
+    for device in d['data']:
+        props = device_info(device)['data']
+#        print(props)
+        temperature = float(props['temperature'].split(' ')[0])
+        target_temperature = float(props['target_temperature'])
+        try:
+            cooling_enabled = bool(int(props['cooling_enabled']))
+        except: 
+            cooling_enabled = False
+
+        if not cooling_enabled: action = 'off'
+
+        elif temperature > temperature:
+           action = 'on'
+
+        else:
+            action = 'off'
+        
+        switch(device, action)
+        print(device, temperature, target_temperature, action)
+
+main()
